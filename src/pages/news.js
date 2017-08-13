e47f1a475a0e8ba9a8618c16bedcde43583949b4
shaunmarx <shaun.m4rx@gmail.com>
2017-08-13 20:39:30 +1000

    Feat(news) add page query

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import PageSection from "~/components/page-section";
+import Typography from '~/components/typography';
 
-export default () => (
-    <PageSection bg="light">
-        <h1>News</h1>
-    </PageSection>
-)
+export default ({data}) =>{ 
+    const news = data ? data.news.edges.map(x => ({ ...x.node.frontmatter, id:x.node.id, slug: x.node.fields.slug, excerpt: x.node.excerpt })) : [];
+
+    return (
+        <PageSection bg="light">
+            <Typography component="h1">News</Typography>
+        </PageSection>
+    )
+}
+
+export const pageQuery = graphql`
+     query NewsQuery {
+        news: allMarkdownRemark(
+        limit: 1000
+        filter:{ fields: { area: { eq: "news"} }}
+        sort: { fields: [frontmatter___date],  order: DESC }
+    ){
+      edges {
+        node{
+          fields{
+            area,
+            slug
+          },
+        id,
+        frontmatter{
+          title,
+          caption,
+          extract,
+          image,
+          thumb,
+          tags
+          }
+       }
+      }
+    }
+  }
+`
