b504fcfa632561c00df58286dc00f21c87b8cd40
Shaun Marx <smarx@internode.on.net>
2017-08-19 14:46:20 +1000

    chore(settings): updated usage to withSettings hoc

--- src/components/social/container.js
+++ src/components/social/container.js
@@ -2,4 +2,4 @@ import React from 'react';
 import  withSettings  from '~/enhancers/with-settings';
 
 const IconsContainer = ({settings, component : Component}) => (<Component icons={ settings.map(x => x.social) }></Component>)
-export default withSettings(IconsContainer);
+export default withSettings()(IconsContainer);
