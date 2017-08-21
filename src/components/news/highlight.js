0b0f8b54990c0aaa1b4b1a56371288e8e29bbd18
shaunmarx <shaun.m4rx@gmail.com>
2017-08-21 14:01:00 +1000

    feat(news): highlight now includes image

--- src/components/news/highlight.js
+++ src/components/news/highlight.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Typography from '~/components/typography';
 import LinkButton from '~/components/link-button';
 import { Box, Flex } from 'rebass';
+import styled from 'styled-components';
 import format from 'date-fns/format';
+
+const StyledImage = styled.img`
+    max-width: 100%;
+`
 
 export default ({className, article, ...rest}) => {
     article = article ? article : {};
-    
+    let image = article.image ? (<StyledImage src={article.image}></StyledImage>) : null;
+
     return (
         <Box className={className} {...rest}>
             <Typography component="h4" pb={0} mb={0}>{article.title}</Typography>
             <Typography f={1} pt={0} mt={1} color="secondaryAccent">{ format(article.date, "DD MMM, YYYY")}</Typography>
+            {image}
             <Typography>{article.excerpt}</Typography>
             <LinkButton href={article.slug}>Read More</LinkButton>
         </Box>
