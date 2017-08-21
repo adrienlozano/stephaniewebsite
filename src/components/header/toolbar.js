1adf942d964bff7cea83c0109cf44b8e50c17fe5
shaunmarx <shaun.m4rx@gmail.com>
2017-08-21 19:14:14 +1000

    feat(header): hide menu icon on larger screens

--- src/components/header/toolbar.js
+++ src/components/header/toolbar.js
@@ -2,23 +2,28 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import { Toolbar } from '~/components/toolbar';
 import { NavLink } from 'rebass';
+import { responsiveStyle } from "styled-system";
 import  Icon  from "~/components/icon";
 
+const MenuNavLink = styled(NavLink)`
+  ${responsiveStyle('display', 'display')};
+`
+
 const toNavIconLinks = (icons) => icons.map( ({name, url}) => (
-        <NavLink key={name} href={url} f={4}><Icon icon={name}></Icon>
-    </NavLink>)
+        <MenuNavLink key={name} href={url} f={4}><Icon icon={name}></Icon>
+    </MenuNavLink>)
 )
 
 const HeaderToolbar = ({contactNumber, icons, onMenuClick}) => {
-  
+
     return (
     <Toolbar bg="secondary" color="white">
-      <NavLink onClick={ onMenuClick } >
+      <MenuNavLink onClick={ onMenuClick } display={['inline-flex', 'inline-flex', 'none']}>
         <Icon icon="menu" f={4}></Icon>
-      </NavLink>
-      <NavLink ml='auto' href={`tel:${contactNumber}`}>
+      </MenuNavLink>
+      <MenuNavLink ml='auto' href={`tel:${contactNumber}`}>
         <Icon icon="phone" f={2} pr={2}></Icon> {contactNumber}
-      </NavLink>
+      </MenuNavLink>
       {toNavIconLinks(icons)}
       </Toolbar>
   )};
