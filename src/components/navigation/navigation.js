d8bc4a5ad33f40accb703656d34be2f41d3d1f94
shaunmarx <shaun.m4rx@gmail.com>
2017-08-21 20:50:41 +1000

    feat(navigation): working by breakpoints

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Flex } from 'rebass';
-import styled from 'styled-components';
-
+import styled, { css } from 'styled-components';
 import { responsiveStyle, color } from 'styled-system';
 import { setDisplayName, compose, defaultProps } from 'recompose';
 
+const hideMenu = (breakpoint) => css`
+  @media (max-width: ${breakpoint}em){
+    display: ${ ({open}) => open ? 'flex' : 'none' };
+  }
+`
+
 const Navigation = styled(Flex)`
     ${ responsiveStyle('flex-direction', 'direction') };
+    ${ ({theme}) => hideMenu(theme.breakpoints[2]) }
 `;
 
 const enhance = compose(
