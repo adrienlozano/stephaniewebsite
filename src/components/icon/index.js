48cb7e8da24f6b65ef6ce670ecab0224b3d33c08
Shaun Marx <shaun.m4rx@gmail.com>
2017-09-24 20:03:05 +1000

    feat(icons): add support for custom icons

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import classNames from 'classnames';
 import { space, fontSize } from 'styled-system';
 import styled from 'styled-components';
+import ParentIcon  from './parent';
+
+const icons = {
+    parent : ParentIcon
+}
 
 var iconClass = (name) => (`mdi mdi-${name}`);
 
 const Icon = ({icon, className}) =>  {
+    if(icon in icons){
+        var CustomIcon = icons.parent;
+        return (<CustomIcon className={className}/>)
+    }
+
     return (<i className={classNames(iconClass(icon), className)}></i>)
 }
 
